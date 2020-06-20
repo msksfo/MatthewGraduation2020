41d5c1b67790acb1df7adb63b4b6217c98773083
Tia Esguerra <msksfo@gmail.com>
2020-06-20 10:41:21 -0700

    Change site title in site metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Matthew Esguerra 2020`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
