96d07d2d4ac82f549df8d0bbdbce2ee29adc4631
Tia Esguerra <msksfo@gmail.com>
2020-06-21 21:24:38 -0700

    Redo the way I'm getting the image urls, because Netlify was returning 404 errors on the images that were in the public folder. Also delete the images that were in public.

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -1,12 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
+
 import Head from "../components/head"
 import Layout from "../components/layout/layout"
 import TimelineEvent from "../components/timelineEvent/timelineEvent"
 import timelineStyles from "../styles/timeline.module.css"
+
 import timelineData from "../data.json"
 
+import birth from "../images/timeline/birth.jpg"
+import walking from "../images/timeline/walking.jpg"
+import withCalista from "../images/timeline/withCalista.jpg"
+import wedding from "../images/timeline/wedding.jpg"
+import withRhi from "../images/timeline/withRhi.jpg"
+import withKekoa from "../images/timeline/withKekoa.jpg"
+import elementaryGrad from "../images/timeline/elementaryGrad.jpg"
+import flying from "../images/timeline/flying.jpg"
+import band from "../images/timeline/band.jpg"
+import drumline from "../images/timeline/drumline.jpg"
+import military from "../images/timeline/military.jpg"
+import matthewcopy from "../images/timeline/matthewcopy.jpg"
+
 // cool timeline inspired by https://codepen.io/Fischaela/pen/sJdqg
-function Timeline() {
+const Timeline = () => {
+  let images = [
+    "",
+    birth,
+    walking,
+    withCalista,
+    wedding,
+    withRhi,
+    withKekoa,
+    elementaryGrad,
+    flying,
+    band,
+    drumline,
+    military,
+    matthewcopy,
+  ]
+
   return (
     <Layout>
       <Head title="The Journey" />
@@ -14,13 +45,14 @@ function Timeline() {
         <h1>This is my life</h1>
         <ul className={timelineStyles.timelineList}>
           {timelineData.map(value => {
+            images = images.slice(1)
             return (
               <TimelineEvent
                 key={value.title}
                 date={value.date}
                 title={value.title}
                 description={value.description}
-                imageUrl={value.imageUrl}
+                imageUrl={images[0]}
                 imageAlt={value.imageAlt}
               />
             )
