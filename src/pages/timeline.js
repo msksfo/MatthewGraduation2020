942fa38c82f34745c522e9b8f5d0b40957a64487
Tia Esguerra <msksfo@gmail.com>
2020-06-21 19:34:00 -0700

    Import json data and create list item components from the data.

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -1,10 +1,9 @@
 import React from "react"
 import Head from "../components/head"
 import Layout from "../components/layout/layout"
+import TimelineEvent from "../components/timelineEvent/timelineEvent"
 import timelineStyles from "../styles/timeline.module.css"
-
-import matthew1 from "../images/matthew1.jpg"
-import matthewGraduates from "../images/matthewcopy.jpg"
+import timelineData from "../data.json"
 
 // cool timeline inspired by https://codepen.io/Fischaela/pen/sJdqg
 function Timeline() {
@@ -14,47 +13,18 @@ function Timeline() {
       <section className={timelineStyles.contentWrapper}>
         <h1>This is my life</h1>
         <ul className={timelineStyles.timelineList}>
-          <li className={timelineStyles.timelineEventWrapper}>
-            <p className={timelineStyles.timelineEventDate}>September 2001</p>
-            <div className={timelineStyles.timelineEventTextWrapper}>
-              <p className={timelineStyles.timelineEventTitle}>I was born!</p>
-              <figure>
-                <img src={matthew1} alt="" />
-                <figcaption className={timelineStyles.eventDescription}>
-                  description of event/photo
-                </figcaption>
-              </figure>
-            </div>
-          </li>
-
-          {/*  make each li into a component??? */}
-          <li className={timelineStyles.timelineEventWrapper}>
-            <p className={timelineStyles.timelineEventDate}>September 2001</p>
-            <div className={timelineStyles.timelineEventTextWrapper}>
-              <p className={timelineStyles.timelineEventTitle}>I was born!</p>
-              <figure>
-                <img src={matthew1} alt="" />
-                <figcaption className={timelineStyles.eventDescription}>
-                  description of event/photo
-                </figcaption>
-              </figure>
-            </div>
-          </li>
-
-          <li className={timelineStyles.timelineEventWrapper}>
-            <p className={timelineStyles.timelineEventDate}>June 2020</p>
-            <div className={timelineStyles.timelineEventTextWrapper}>
-              <p className={timelineStyles.timelineEventTitle}>
-                I graduated from high school!
-              </p>
-              <figure>
-                <img src={matthewGraduates} alt="" />
-                <figcaption className={timelineStyles.eventDescription}>
-                  One journey ends. Another begins.
-                </figcaption>
-              </figure>
-            </div>
-          </li>
+          {timelineData.map(value => {
+            return (
+              <TimelineEvent
+                key={value.title}
+                date={value.date}
+                title={value.title}
+                description={value.description}
+                imageUrl={value.imageUrl}
+                imageAlt={value.imageAlt}
+              />
+            )
+          })}
         </ul>
       </section>
     </Layout>
